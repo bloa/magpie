037f9e78583ba416fde191cf2734b563ebf931a2
Aymeric Blot <blot.aymeric@gmail.com>
2020-09-17 15:09:27 +0100

    expose pyggi.tree.TreeEdit

--- pyggi/tree/__init__.py
+++ pyggi/tree/__init__.py
@@ -2,4 +2,4 @@ from .abstract_engine import AbstractTreeEngine
 from .astor_engine import AstorEngine
 from .xml_engine import XmlEngine
 from .tree import StmtReplacement, StmtInsertion, StmtDeletion, StmtMoving
-from .tree import TreeProgram
+from .tree import TreeEdit, TreeProgram
