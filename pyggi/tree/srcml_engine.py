921cad1a33237c0827cb02df6e24e1e941884310
Aymeric Blot <blot.aymeric@gmail.com>
2020-11-06 12:40:56 +0000

    add srcml specific handler

---
+++ pyggi/tree/srcml_engine.py
@@ -0,0 +1,66 @@
+import re
+from xml.etree import ElementTree
+
+from . import XmlEngine
+
+class SrcmlEngine(XmlEngine):
+    TAG_RENAME = {
+        'stmt': {'break', 'continue', 'decl_stmt', 'do', 'expr_stmt', 'for', 'goto', 'if', 'return', 'switch', 'while'},
+        'operator_comp': {'operator_comp'},
+        'operator_arith': {'operator_arith'},
+        'number': {'literal_number'}
+    }
+    TAG_FOCUS = { t for tl in TAG_RENAME.values() for t in tl }
+    PROCESS_PSEUDO_BLOCKS = True
+    PROCESS_LITERALS = True
+    PROCESS_OPERATORS = True
+
+    @classmethod
+    def process_tree(cls, tree):
+        if cls.PROCESS_PSEUDO_BLOCKS:
+            cls.process_pseudo_blocks(tree)
+        if cls.PROCESS_CONDITIONS:
+            cls.process_conditions(tree)
+        if cls.PROCESS_LITERALS:
+            cls.process_literals(tree)
+        if cls.PROCESS_OPERATORS:
+            cls.process_operators(tree)
+        if len(cls.TAG_FOCUS) > 0:
+            cls.focus_tags(tree, cls.TAG_FOCUS)
+        for tag in cls.TAG_RENAME:
+            cls.rewrite_tags(tree, cls.TAG_RENAME[tag], tag)
+
+    @classmethod
+    def process_pseudo_blocks(cls, element, sp_element=''):
+        sp = cls.guess_spacing(element.text)
+        for child in element:
+            cls.process_pseudo_blocks(child, sp)
+            sp = cls.guess_spacing(child.tail)
+        if element.tag == 'block' and element.attrib.get('type') == 'pseudo':
+            del element.attrib['type']
+            if len(element) > 0:
+                element.text = '/*auto*/{' + (element.text or '')
+                child.tail = (child.tail or '') + '\n' + sp_element + '}/*auto*/'
+            else:
+                element.text = '/*auto*/{' + (element.text or '') + '}/*auto*/'
+
+    @classmethod
+    def process_literals(cls, element):
+        for child in element:
+            cls.process_literals(child)
+        if element.tag == 'literal':
+            element.tag = 'literal_{}'.format(element.attrib.get('type'))
+            del element.attrib['type']
+
+    @classmethod
+    def process_operators(cls, element):
+        for child in element:
+            cls.process_operators(child)
+        if element.tag == 'operator':
+            # TODO
+            if element.text in ['==', '!=', '<', '<=', '>', '>=']:
+                element.tag = 'operator_comp'
+            elif element.text in ['+', '-', '*', '/']:
+                element.tag = 'operator_arith'
+            else:
+                element.tag = 'operator_misc'
