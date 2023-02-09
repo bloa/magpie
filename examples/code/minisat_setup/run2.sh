99c4cb4c96f86817b0ffa96bed6393fd27a1491b
Aymeric Blot <blot.aymeric@gmail.com>
2023-02-10 00:22:49 +0100

    update examples

--- examples/code/minisat_setup/run2.sh
+++ examples/code/minisat_setup/run2.sh
@@ -1,40 +1,43 @@
 #!/bin/sh
 
 # handle the two artificial parameters of minisat_advanced.params
+ARGV=""
 for arg in "$@"; do
-    shift
+    echo $arg
     case "$arg" in
         # if positive, then use -1 for the original parameter
-        "-sub-lim-unbounded") shift ; set -- "$@" "-sub-lim" "-1" ;;
-        "-cl-lim-unbounded") shift ; set -- "$@" "-cl-lim" "-1" ;;
+        "-sub-lim-unbounded") ARGV="$ARGV -sub-lim=-1" ;;
+        "-cl-lim-unbounded") ARGV="$ARGV -cl-lim=-1" ;;
         # if negative, delete
-        "-no-sub-lim-unbounded") shift ;;
-        "-no-cl-lim-unbounded") shift ;;
+        "-no-sub-lim-unbounded") ;;
+        "-no-cl-lim-unbounded") ;;
         # don't touch other parameters
-        *)
+        *) ARGV="$ARGV $arg";;
     esac
+    echo $ARGV
+    shift
 done
 
-./simp/minisat data/uf50-01.cnf $@
-./simp/minisat data/uf50-02.cnf $@
-./simp/minisat data/uf100-01.cnf $@
-./simp/minisat data/uf100-02.cnf $@
-./simp/minisat data/uf150-01.cnf $@
-./simp/minisat data/uf150-02.cnf $@
-./simp/minisat data/uf200-01.cnf $@
-./simp/minisat data/uf200-02.cnf $@
-./simp/minisat data/uf250-01.cnf $@
-./simp/minisat data/uf250-02.cnf $@
+./simp/minisat data/uf50-01.cnf $ARGV
+./simp/minisat data/uf50-02.cnf $ARGV
+./simp/minisat data/uf100-01.cnf $ARGV
+./simp/minisat data/uf100-02.cnf $ARGV
+./simp/minisat data/uf150-01.cnf $ARGV
+./simp/minisat data/uf150-02.cnf $ARGV
+./simp/minisat data/uf200-01.cnf $ARGV
+./simp/minisat data/uf200-02.cnf $ARGV
+./simp/minisat data/uf250-01.cnf $ARGV
+./simp/minisat data/uf250-02.cnf $ARGV
 
-./simp/minisat data/uuf50-01.cnf $@
-./simp/minisat data/uuf50-02.cnf $@
-./simp/minisat data/uuf100-01.cnf $@
-./simp/minisat data/uuf100-02.cnf $@
-./simp/minisat data/uuf150-01.cnf $@
-./simp/minisat data/uuf150-02.cnf $@
-./simp/minisat data/uuf200-01.cnf $@
-./simp/minisat data/uuf200-02.cnf $@
-./simp/minisat data/uuf250-01.cnf $@
-./simp/minisat data/uuf250-02.cnf $@
+./simp/minisat data/uuf50-01.cnf $ARGV
+./simp/minisat data/uuf50-02.cnf $ARGV
+./simp/minisat data/uuf100-01.cnf $ARGV
+./simp/minisat data/uuf100-02.cnf $ARGV
+./simp/minisat data/uuf150-01.cnf $ARGV
+./simp/minisat data/uuf150-02.cnf $ARGV
+./simp/minisat data/uuf200-01.cnf $ARGV
+./simp/minisat data/uuf200-02.cnf $ARGV
+./simp/minisat data/uuf250-01.cnf $ARGV
+./simp/minisat data/uuf250-02.cnf $ARGV
 
 exit 0
