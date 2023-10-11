b485cc2d8320ec304a90cc87bf79241c32723d51
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2023-10-11 16:49:04 +0200

    reformat pytest code to use parameterised tests

--- examples/code/triangle-py_bloat/test_triangle.py
+++ examples/code/triangle-py_bloat/test_triangle.py
@@ -1,30 +1,46 @@
-import time
 import pytest
 from triangle import TriangleType, classify_triangle
 
 
-def check_classification(triangles, expected_result):
-    for triangle in triangles:
-        assert classify_triangle(*triangle) == expected_result
+@pytest.mark.parametrize('triangle', [
+    (1, 2, 9),
+    (1, 9, 2),
+    (2, 1, 9),
+    (2, 9, 1),
+    (9, 1, 2),
+    (9, 2, 1),
+    (1, 1, -1),
+    (1, -1, 1),
+    (-1, 1, 1),
+])
+def test_invalid_triangles(triangle):
+    assert classify_triangle(*triangle) == TriangleType.INVALID
+
+@pytest.mark.parametrize('triangle', [
+    (1, 1, 1),
+    (99, 99, 99),
+    (100, 100, 100),
+])
+def test_equilateral_triangles(triangle):
+    assert classify_triangle(*triangle) == TriangleType.EQUILATERAL
 
 
-def test_invalid_triangles():
-    triangles = [(1, 2, 9), (1, 9, 2), (2, 1, 9), (2, 9, 1), (9, 1, 2),
-                 (9, 2, 1), (1, 1, -1), (1, -1, 1), (-1, 1, 1)]
-    check_classification(triangles, TriangleType.INVALID)
+@pytest.mark.parametrize('triangle', [
+    (100, 90, 90),
+    (90, 100, 90),
+    (90, 90, 100),
+    (2, 2, 3),
+])
+def test_isosceles_triangles(triangle):
+    assert classify_triangle(*triangle) == TriangleType.ISOSCELES
 
 
-def test_equalateral_triangles():
-    triangles = [(1, 1, 1), (100, 100, 100), (99, 99, 99)]
-    check_classification(triangles, TriangleType.EQUILATERAL)
-
-
-def test_isosceles_triangles():
-    triangles = [(100, 90, 90), (90, 100, 90), (90, 90, 100), (2, 2, 3)]
-
-    check_classification(triangles, TriangleType.ISOSCELES)
-
-
-def test_scalene_triangles():
-    triangles = [(5, 4, 3), (5, 3, 4), (4, 5, 3), (4, 3, 5), (3, 5, 4)]
-    check_classification(triangles, TriangleType.SCALENE)
+@pytest.mark.parametrize('triangle', [
+    (5, 4, 3),
+    (5, 3, 4),
+    (4, 5, 3),
+    (4, 3, 5),
+    (3, 5, 4)
+])
+def test_scalene_triangles(triangle):
+    assert classify_triangle(*triangle) == TriangleType.SCALENE
