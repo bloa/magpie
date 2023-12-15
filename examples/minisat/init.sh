e04fbc7c541ad47d8cc15d24a56e250c38acbc0e
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2023-12-15 17:27:04 +0100

    improve minisat example

--- examples/minisat/init.sh
+++ examples/minisat/init.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 CACHE=$MAGPIE_ROOT/_magpie_cache
+ARCHIVE=minisat-2.2.0.tar.gz
 
 # download and cache archive
 mkdir -p $CACHE
-if ! test -f $CACHE/minisat-2.2.0.tar.gz; then
-    wget "http://minisat.se/downloads/minisat-2.2.0.tar.gz"
+if ! test -f $CACHE/$ARCHIVE; then
+    wget "http://minisat.se/downloads/$ARCHIVE"
+    mv $ARCHIVE $CACHE
 fi
-mv minisat-2.2.0.tar.gz $CACHE
 
 # extract, patch, and setup pre-computed XML AST
-tar xzf $CACHE/minisat-2.2.0.tar.gz --strip-components=1
+tar xzf $CACHE/$ARCHIVE --strip-components=1
 patch core/Dimacs.h _magpie/dimacs.diff
 # srcml core/Solver.cc > _magpie/Solver.cc.xml
-mv _magpie/Solver.cc.xml core
+cp _magpie/Solver.cc.xml core
