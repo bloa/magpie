bloa/magpie

commit ca0b2fe53c59b1d83bc09be7379c68340e9df42f

simplify python_to_xml

magpie/scripts/python_to_xml.py
@@ -1,22 +1,9 @@
 import argparse
 import ast
+import pathlib
+import sys
 from ast import *
 from contextlib import contextmanager, nullcontext
-import sys
-
-def read_file_or_stdin(filename):
-    if filename == 'stdin':
-        return sys.stdin.read()
-    with open(filename) as f:
-        return f.read()
-
-def unparse_xml(root, filename=""):
-    unparser = XmlUnparser()
-    output = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-    output += f"<unit filename=\"{filename}\">\n"
-    output += unparser.visit(root)
-    output += "\n</unit>\n"
-    return output
 
 
 # mostly lifted from cpython/Lib/ast.py
@@ -270,6 +257,16 @@ if __name__ == "__main__":
     parser.add_argument('file', default='stdin', nargs='?')
     args = parser.parse_args()
 
-    contents = read_file_or_stdin(args.file)
-    root = ast.parse(contents+'\n')
-    print(unparse_xml(root, args.file))
+    if args.file == 'stdin':
+        full = sys.stdin.read()
+    else:
+        with pathlib.Path(args.file).open('r') as f:
+            full = f.read()
+    root = ast.parse(full+'\n')
+
+    unparser = XmlUnparser()
+    output = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+    output += f"<unit filename=\"{args.file}\">\n"
+    output += unparser.visit(root)
+    output += "\n</unit>\n"
+    print(output)

commit 7f031d9cefe3836b7f71fd87ab0303c90c20d0f7

fix regression caused by ca0b2fe

magpie/scripts/python_to_xml.py
@@ -5,6 +5,19 @@ import sys
 from ast import *
 from contextlib import contextmanager, nullcontext
 
+def read_file_or_stdin(filename):
+    if filename == 'stdin':
+        return sys.stdin.read()
+    with pathlib.Path(filename).open('r') as f:
+        return f.read()
+
+def unparse_xml(root, filename=""):
+    return f"""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<unit filename="{filename}">
+{XmlUnparser().visit(root)}
+</unit>
+"""
+
 
 # mostly lifted from cpython/Lib/ast.py
 # see https://docs.python.org/3/library/ast.html#abstract-grammar
@@ -257,16 +270,6 @@ if __name__ == "__main__":
     parser.add_argument('file', default='stdin', nargs='?')
     args = parser.parse_args()
 
-    if args.file == 'stdin':
-        full = sys.stdin.read()
-    else:
-        with pathlib.Path(args.file).open('r') as f:
-            full = f.read()
-    root = ast.parse(full+'\n')
-
-    unparser = XmlUnparser()
-    output = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-    output += f"<unit filename=\"{args.file}\">\n"
-    output += unparser.visit(root)
-    output += "\n</unit>\n"
-    print(output)
+    contents = read_file_or_stdin(args.file)
+    root = ast.parse(contents+'\n')
+    print(unparse_xml(root, args.file))