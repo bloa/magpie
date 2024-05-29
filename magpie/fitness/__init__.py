1a2f729e78e2df043482637745daf88866dca0b0
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2024-05-29 15:26:29 +0200

    add GNU /usr/bin/time support

--- magpie/fitness/__init__.py
+++ magpie/fitness/__init__.py
@@ -1,4 +1,19 @@
 from .bloat import BloatCharsFitness, BloatLinesFitness, BloatWordsFitness
+from .gnu_time import (
+    GnuMajorPagefaultsFitness,
+    GnuMemoryFitness,
+    GnuMinorPagefaultsFitness,
+    GnuPagefaultsFitness,
+    GnuSwapsFitness,
+    GnuSystemTimeFitness,
+    GnuTimeFitness,
+    GnuUserTimeFitness,
+)
 from .output import OutputFitness
 from .repair import RepairFitness
-from .time import PerfInstructionsFitness, PerfTimeFitness, PosixTimeFitness, TimeFitness
+from .time import (
+    PerfInstructionsFitness,
+    PerfTimeFitness,
+    PosixTimeFitness,
+    TimeFitness,
+)
