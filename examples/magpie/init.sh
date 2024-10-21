4e6ad2a5cc0c8850b55cd965d736e51ae5caf9be
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2024-10-21 18:44:46 +0200

    fix magpie on magpie scenario example

--- examples/magpie/init.sh
+++ examples/magpie/init.sh
@@ -2,9 +2,18 @@
 
 shopt -s globstar
 
+# clean up just in case
 bash clean.sh
-rsync --archive ../../../{examples,magpie,tests} .
-for file in **/*.py;
-do
+
+# the relative magpie root is different when executed manually from "example/magpie"
+parent_dir=$(basename "$(dirname "$(pwd)")")
+if [ "$parent_dir" == "examples" ]; then
+    rsync --archive ../../{examples,magpie,tests} .
+    else
+    rsync --archive ../../../{examples,magpie,tests} .
+fi
+
+# store the AST of every file
+for file in **/*.py; do
     python3 magpie utils/python_to_xml $file > $file.xml
 done
