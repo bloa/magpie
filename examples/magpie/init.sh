b27c213ebd19c40a6a7349adcda4b6f3036dfe8e
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2024-10-21 18:46:27 +0200

    magpie on magpie example requires python_to_xml.py

--- examples/magpie/init.sh
+++ examples/magpie/init.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-shopt -s globstar
-
 bash clean.sh
 rsync --archive ../../../{examples,magpie,tests} .
-for file in **/*.py;
-do
-    python3 magpie utils/python_to_xml $file > $file.xml
-done
