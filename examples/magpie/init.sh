f417719fbfa1140579ed7584a9babd66fa3c1de9
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2024-10-21 18:46:27 +0200

    magpie on magpie example requires python_to_xml.py

--- examples/magpie/init.sh
+++ examples/magpie/init.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 
-shopt -s globstar
-
-# clean up just in case
 bash clean.sh
-
-# the relative magpie root is different when executed manually from "example/magpie"
-parent_dir=$(basename "$(dirname "$(pwd)")")
-if [ "$parent_dir" == "examples" ]; then
-    rsync --archive ../../{examples,magpie,tests} .
-    else
-    rsync --archive ../../../{examples,magpie,tests} .
-fi
-
-# store the AST of every file
-for file in **/*.py; do
-    python3 magpie utils/python_to_xml $file > $file.xml
-done
+rsync --archive ../../../{examples,magpie,tests} .
