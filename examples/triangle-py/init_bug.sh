e6e97a06ade0d6714ce45113075c5f6312756f22
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2025-04-22 17:11:56 +0200

    some triangle-py examples require python_to_xml.py

--- examples/triangle-py/init_bug.sh
+++ examples/triangle-py/init_bug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 patch triangle.py _magpie/triangle_bug.diff
-python3 ../../../magpie python_to_xml triangle.py > triangle.py.xml
+cp _magpie/triangle_bug.py.xml triangle.py.xml
