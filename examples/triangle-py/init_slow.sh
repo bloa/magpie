f3810d47dadd16f787ace784736f89d60c18fdab
Aymeric Blot <aymeric.blot@univ-rennes.fr>
2025-04-22 17:11:56 +0200

    some triangle-py examples require python_to_xml.py

--- examples/triangle-py/init_slow.sh
+++ examples/triangle-py/init_slow.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 patch triangle.py _magpie/triangle_slow.diff
-python3 ../../../magpie python_to_xml triangle.py > triangle.py.xml
+cp _magpie/triangle_slow.py.xml triangle.py.xml
